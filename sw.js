2e033c7685a25015864bab5d6d15643814b0900c
nanakiomi <yujielin861@gmail.com>
2026-03-19 02:53:49 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,2 @@
-const CACHE = 'scheduler-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const resClone = res.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, resClone));
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
